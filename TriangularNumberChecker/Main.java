c8cd89c9d0e699089cfc1588678236ec8ee599ff
Dheeraj Bhatia <dheerajbhatia2010@gmail.com>
2018-08-23 02:25:43 +0530

    Triangular No checker
    The code checks whether the no is triangular no or not. It uses Scanner
    class of Util package to take input from command line.

---
+++ TriangularNumberChecker/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        System.out.print("Hello this is a isTriangularNumber checker by Dheeraj Bhatia"+"\n"+"Add a no to check whether it is a TriangularNumber or not");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        System.out.println("You entered integer "+a);
+        isTriangularNumber(a);
+    }
+
+    public static boolean isTriangularNumber(long num) {
+     long calc_num = 8*num+1;
+     long t = (long) Math.sqrt(calc_num);
+     if (t*t==calc_num) {
+                System.out.println("It is a Triangular Number ");
+        return true;
+     }else {
+             System.out.println("It is not a Triangular Number ");  
+                  return false;
+     }
+}
+}
